0aaaeb7d8e74488f8e61f30fed2ceb9526b56c6b
Ihor Bats <batsihor@gmail.com>
2013-06-12 17:26:44 +0300

    Initial commit.
    Applications with code for 2.2 and 2.3 lections

---
+++ Quick FindUF_2_2/Quick FindUF_2_2/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Quick_FindUF_2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QuickFindUF quickFind = new QuickFindUF();
+
+            // Connecting
+            while (true)
+            {
+                string readLine = Console.ReadLine();
+                int p = int.Parse(readLine);
+
+                string readLine1 = Console.ReadLine();
+                int q = int.Parse(readLine1);
+
+                //ExitPoint
+                if(p == -1)
+                    break;
+                
+                quickFind.Union(p, q);
+            
+            }
+
+            // Check if points are connected.
+            string readLine2 = Console.ReadLine();
+            int p1 = int.Parse(readLine2);
+
+            string readLine3 = Console.ReadLine();
+            int q1 = int.Parse(readLine3);
+
+            if (quickFind.IsConnected(p1, q1))
+            {
+                Console.WriteLine("Connected");
+                Console.ReadLine();
+            }
+        }
+    }
+
+    class QuickFindUF
+    {
+        int[] id = new int[10];
+
+        public QuickFindUF()
+        {
+            for (int i = 0; i < id.Length; i++)
+            {
+                id[i] = i;
+            }
+        }
+
+        public bool IsConnected(int p, int q)
+        {
+            return id[p] == id[q];
+        }
+
+        public void Union(int p, int q) 
+        {
+            int pid = id[p];
+            int qid = id[q];
+            for (int i = 0; i < id.Length; i++)
+            {
+                if (id[i] == pid)
+                {
+                    id[i] = qid;
+                }
+                Console.Write("{0} ", id[i]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Quick_Union_2_3/Quick_Union_2_3/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace Quick_Union_2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QuickUnion quickUnion = new QuickUnion();
+
+            // Connecting
+            while (true)
+            {
+                string readLine = Console.ReadLine();
+                int p = int.Parse(readLine);
+                int pRoot = quickUnion.FindRoot(p);
+
+                string readLine1 = Console.ReadLine();
+                int q = int.Parse(readLine1);
+                int qRoot = quickUnion.FindRoot(q);
+
+                //ExitPoint
+                if (p == -1)
+                    break;
+
+                quickUnion.Union(pRoot, qRoot);
+
+            }
+
+            // Check if points are connected.
+            string readLine2 = Console.ReadLine();
+            int p1 = int.Parse(readLine2);
+
+            string readLine3 = Console.ReadLine();
+            int q1 = int.Parse(readLine3);
+
+            if (quickUnion.IsConnected(p1, q1))
+            {
+                Console.WriteLine("Connected");
+                Console.ReadLine();
+            }
+        }
+    }
+
+    internal class QuickUnion
+    {
+        int[] id = new int[10];
+
+        public QuickUnion()
+        {
+            for (int i = 0; i < id.Length; i++)
+            {
+                id[i] = i;
+            }
+        }
+
+        public void Union(int p, int q)
+        {
+            id[p] = id[q];
+            
+            for (int i = 0; i < id.Length; i++)
+            {
+                Console.Write("{0} ", id[i]);
+            }
+            Console.WriteLine();
+        }
+
+        public bool IsConnected(int p, int q)
+        {
+            return id[p] == id[q];
+        }
+
+        public int FindRoot(int item)
+        {
+
+            while (item != id[item])
+            {
+                item = id[item];
+            }
+
+            Console.WriteLine("Root {0}", item);
+            return item;
+        }
+    }
+}
